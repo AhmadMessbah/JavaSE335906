76bcb93b1e78fe0949791937f3acdc788cff172c
narges hajizadeh <narcissus.h2005@gmail.com>
2025-05-20 19:03:50 +0330

    Update NargesHajizdeh.java

--- src/main/java/mft/NargesHajizdeh.java
+++ src/main/java/mft/NargesHajizdeh.java
@@ -3,6 +3,6 @@ package mft;
 public class NargesHajizdeh {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
-        System.out.println("I am Narges Hajizadeh");
+        System.out.println("I'm Narges Hajizadeh");
     }
 }
