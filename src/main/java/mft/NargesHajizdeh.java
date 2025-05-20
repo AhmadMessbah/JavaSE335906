e700e1614d2dba0b25fd06a7077f4cf14663ff4e
Narges <narcissus.h2005@gmail.com>
2025-05-20 19:09:43 +0330

    NargesHajizdeh-Test

--- src/main/java/mft/NargesHajizdeh.java
+++ src/main/java/mft/NargesHajizdeh.java
@@ -1,8 +1,11 @@
 package mft;
 
+import javax.sound.midi.Soundbank;
+
 public class NargesHajizdeh {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
         System.out.println("I am Narges Hajizadeh");
+        System.out.println("test");
     }
 }
